3340f722729109f3e6a4b5283138e525eda3d1d2
Jonathan Liera <jonliera1@gmail.com>
2023-03-24 20:32:26 -0500

    refactor our app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import CoffeeList from "./CoffeeList";
 
 const App = () => {
   return (
     <React.Fragment>
       <Header />
+      <CoffeeList />
     </React.Fragment>
   );
 };
