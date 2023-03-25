57f2d2fd69e00f7856da9efb2e3902dbb9e6664b
Jonathan Liera <jonliera1@gmail.com>
2023-03-25 08:34:58 -0400

    added a newcoffee component with a form

---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewCoffeeForm = (props) => {
+  return (
+    <React.Fragment>
+      <h3>This is a new coffee form</h3>
+    </React.Fragment>
+  );
+};
+
+export default NewCoffeeForm;
