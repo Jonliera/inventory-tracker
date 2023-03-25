dfdd568a0eff9bc27f2b5171ffad7483a50a7242
Jonathan Liera <jonliera1@gmail.com>
2023-03-24 20:36:30 -0500

    refactor out coffeelist component

--- src/components/CoffeeList.js
+++ src/components/CoffeeList.js
@@ -2,7 +2,14 @@ import React from "react";
 import Coffee from "./Coffee";
 
 const CoffeeList = () => {
-  return <Coffee />;
+  return (
+    <Coffee
+      name="Coffee Name"
+      origin="Coffee Origin"
+      price="Coffee Price"
+      roast="Coffee Roast"
+    />
+  );
 };
 
 export default CoffeeList;
