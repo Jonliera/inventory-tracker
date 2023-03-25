ac4d9c8c74ebb00fe9d56bebe8b52600363909cd
Jonathan Liera <jonliera1@gmail.com>
2023-03-25 09:10:48 -0400

    refactor code in our coffeform

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -3,7 +3,13 @@ import React from "react";
 const NewCoffeeForm = (props) => {
   return (
     <React.Fragment>
-      <h3>This is a new coffee form</h3>
+      <form onSubmit={handleNewCoffeeFormSubmission}>
+        <input type="text" name="name" placeholder="Coffee Name" />
+        <input type="text" name="origin" placeholder="Coffee Origin" />
+        <input type="text" name="price" placeholder="Coffee Price" />
+        <input type="text" name="roast" placeholder="Coffee Roast" />
+        <button type="submit">Add</button>
+      </form>
     </React.Fragment>
   );
 };
