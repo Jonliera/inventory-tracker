311dd03f5be5b75e221c3589abb07a0443e290c2
Jonathan Liera <jonliera1@gmail.com>
2023-03-24 20:39:16 -0500

    add props to our coffee component

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -1,18 +1,14 @@
 import React from "react";
 import PropTyoes from "prop-types";
 
-const Coffee = () => {
-  const name = "Coffee Name";
-  const origin = "Coffee Origin";
-  const price = "Coffee Price";
-  const roast = "Coffee Roast";
-
+const Coffee = (props) => {
   return (
     <React.Fragment>
-      <h3>{name}</h3>
-      <p>{origin}</p>
-      <p>{price}</p>
-      <p>{roast}</p>
+      <h3>{props.name}</h3>
+      <p>{props.origin}</p>
+      <p>{props.price}</p>
+      <p>{props.roast}</p>
+      <hr />
     </React.Fragment>
   );
 };
