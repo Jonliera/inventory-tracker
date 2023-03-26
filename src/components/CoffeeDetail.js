a87a86aac7501921ec5abc47c1124c362bf632e8
Jonathan Liera <jonliera1@gmail.com>
2023-03-25 22:55:25 -0400

    add our function to sell coffee

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -1,16 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const CoffeeDetail = (props) => {
   const { coffee, onClickingDelete } = props;
+  const [quantity, setQuantity] = useState(130);
+  let decrementQuantity = () => setQuantity(quantity - 1);
+  if (quantity <= 1 || quantity == "Out of Stock") {
+    decrementQuantity = () => setQuantity("Out of Stock");
+  }
 
   return (
     <React.Fragment>
       <h1>Coffee Details</h1>
       <h3>{coffee.name}</h3>
       <h3>{coffee.origin}</h3>
-      <h3>{coffee.price}</h3>
+      <h3>Price per lb {coffee.price}</h3>
       <h3>{coffee.roast}</h3>
+      <h3>Available in lbs :{quantity}</h3>
+      <button onClick={decrementQuantity}>Buy Coffee</button>
       <button onClick={props.onClickingEdit}>Update Coffee</button>
       <button onClick={() => onClickingDelete(coffee.id)}>Delete Coffee</button>
       <hr />
@@ -20,6 +28,8 @@ const CoffeeDetail = (props) => {
 
 CoffeeDetail.propTypes = {
   coffee: PropTypes.object,
+  quantity: PropTypes.number,
+  decrementQuantity: PropTypes.func,
   onClickingDelete: PropTypes.func,
   onClickingEdit: PropTypes.func,
 };
